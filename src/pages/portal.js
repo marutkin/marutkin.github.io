f4554cbf31ab449889acc34f57ce7f60d9808ce3
101officers <89541175+101officers@users.noreply.github.com>
2021-08-27 15:20:36 +0300

    feature: better layout

--- src/pages/portal.js
+++ src/pages/portal.js
@@ -7,56 +7,54 @@ import Media from "../components/Media";
 const { Meta } = Card;
 const { Content } = Layout;
 
-const PortalPage = () => {
-  return (
-    <Layout style={{ padding: "15px" }}>
-      <Content>
-        <Space
-          style={{ width: "100%" }}
-          align="center"
-          direction="vertical"
-          size="small"
-        >
-          <Link to="/strategy">
-            <Card
-              style={{ width: 345 }}
-              hoverable
-              cover={
-                <img
-                  alt="strategy"
-                  src="https://upload.wikimedia.org/wikipedia/commons/0/03/CH-47_Chinook_helicopter_flyby.jpg"
-                />
-              }
-            >
-              <Meta
-                title="Command"
-                description="Основные направления развития комманды и статус текущих задач для офицерского состава"
+const PortalPage = () => (
+  <Layout style={{ padding: "15px" }}>
+    <Content>
+      <Space
+        style={{ width: "100%" }}
+        align="center"
+        direction="vertical"
+        size="small"
+      >
+        <Link to="/strategy">
+          <Card
+            style={{ width: 345 }}
+            hoverable
+            cover={
+              <img
+                alt="strategy"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/03/CH-47_Chinook_helicopter_flyby.jpg"
               />
-            </Card>
-          </Link>
+            }
+          >
+            <Meta
+              title="Command"
+              description="Основные направления развития комманды и статус текущих задач для офицерского состава"
+            />
+          </Card>
+        </Link>
 
-          <Link to="/officers">
-            <Card
-              style={{ width: 345 }}
-              hoverable
-              cover={
-                <img alt="officers" src="https://i.redd.it/v62t498mf0j51.jpg" />
-              }
-            >
-              <Meta
-                title="Officers"
-                description="Список офицеров со списком личных задач и обязанностей"
-              />
-            </Card>
-          </Link>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <Media />
-          </div>
-        </Space>
-      </Content>
-      <BackTop visibilityHeight={30} />
-    </Layout>
-  );
-};
+        <Link to="/officers">
+          <Card
+            style={{ width: 345 }}
+            hoverable
+            cover={
+              <img alt="officers" src="https://i.redd.it/v62t498mf0j51.jpg" />
+            }
+          >
+            <Meta
+              title="Officers"
+              description="Список офицеров со списком личных задач и обязанностей"
+            />
+          </Card>
+        </Link>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Media />
+        </div>
+      </Space>
+    </Content>
+    <BackTop visibilityHeight={30} />
+  </Layout>
+);
 
 export default PortalPage;
--- src/pages/strategy.js
+++ src/pages/strategy.js
@@ -6,18 +6,16 @@ import Steps from "../components/Steps";
 
 const { Content } = Layout;
 
-const StrategyPage = () => {
-  return (
-    <Layout style={{padding: '15px'}}>
-      <Content>
-        <Steps />
-        <Divider />
-        <Strategy />
-        <Divider />
-      </Content>
-      <BackTop visibilityHeight={30} />
-    </Layout>
-  );
-};
+const StrategyPage = () => (
+  <Layout style={{ padding: "15px" }}>
+    <Content>
+      <Steps />
+      <Divider />
+      <Strategy />
+      <Divider />
+    </Content>
+    <BackTop visibilityHeight={30} />
+  </Layout>
+);
 
 export default StrategyPage;
